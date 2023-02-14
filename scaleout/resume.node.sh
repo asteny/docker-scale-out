7c5ab47d245e48ad6d442fba0c7b5d6e76347541
Nathan Rini <nate@schedmd.com>
2023-02-14 10:25:42 -0700

    Verify cloud script sent ACK

--- scaleout/resume.node.sh
+++ scaleout/resume.node.sh
@@ -5,7 +5,9 @@ exec 1> >(logger -s -t $(basename $0)) 2>&1
 
 scontrol show hostnames $1 | while read nodename
 do
-	echo "start:$nodename" | socat -t9999 STDIO UNIX-CONNECT:/run/cloud_socket
+	resp=$(echo "start:$nodename" | socat -t9999 STDIO UNIX-CONNECT:/run/cloud_socket)
+
+	[ "$resp" != "ACK" ] && exit 1
 done
 
 true
