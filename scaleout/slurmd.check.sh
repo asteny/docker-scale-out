756ea7fe38e09bfab4e516e05c1263fd5d1ea8b5
Nathan Rini <nate@schedmd.com>
2024-03-18 17:06:55 -0600

    Remove cloning environment in slurmd.check.sh
    Not needed anymore with service unit drop ins

--- scaleout/slurmd.check.sh
+++ scaleout/slurmd.check.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 exec 1> >(logger -s -t $(basename $0)) 2>&1
 
-# Import our environment variables from systemd
-# https://unix.stackexchange.com/questions/146995/inherit-environment-variables-in-systemd-docker-container
-for e in $(tr "\000" "\n" < /proc/1/environ); do
-        eval "export $e"
-done
-
 # always load on a cloud node
 [ "$CLOUD" ] && exit 0
 
