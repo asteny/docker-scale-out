a27713844dcd7fdf2ecedd10fccf707785e1e491
Nathan Rini <nate@schedmd.com>
2024-07-10 12:23:24 -0600

    Delay slurmrestd startup until after /auth/slurm populated
    Avoid calling 'scontrol' with SLURM_JWT set causing slurmctld to get a
    pile of invalid RPCs.

--- scaleout/slurmrestd.startup.sh
+++ scaleout/slurmrestd.startup.sh
@@ -4,7 +4,7 @@ mkdir -p -m 0755 /run/slurm
 chown slurmrestd:slurmrestd -R /run/slurm
 
 #wait for primary mgt node to be done starting up
-while [[ "$(scontrol --json ping | jq -r '.pings[0].pinged')" != "UP" ]]
+while [[ ! -s /auth/slurm ]]
 do
 	sleep 0.25
 done
